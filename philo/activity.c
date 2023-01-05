d2f8cbb789ec7f81e5e7507962bef693c95ec081
GANZIII <108858693+GANZIII@users.noreply.github.com>
2023-01-05 17:29:13 +0900

    final

--- philo/activity.c
+++ philo/activity.c
@@ -1,40 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   activity.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jijoo <jijoo@student.42seoul.kr>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/31 13:37:01 by jijoo             #+#    #+#             */
-/*   Updated: 2023/01/04 14:50:40 by jijoo            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "./include/philosopher.h"
 
-void    philo_eat(t_argset *arg, t_philo *philo)
+void	philo_eat(t_argset *arg, t_philo *philo)
 {
-    pthread_mutex_lock(&(arg->fork[(philo->fork_left)]));
-    display(arg, "has taken a fork", philo->id);
-    if (arg->num_philo > 1)
-    {
-        pthread_mutex_lock(&(arg->fork[philo->fork_right]));
-        display(arg, "has taken a fork", philo->id);
-        display(arg, "is eating", philo->id);
-        pthread_mutex_lock(&philo->last_meal_mutex);
-        philo->last_meal = get_time();
-        pthread_mutex_unlock(&philo->last_meal_mutex);
-        pthread_mutex_lock(&philo->cnt_eat_mutex);
-        philo->cnt_eat += 1;
-        pthread_mutex_unlock(&philo->cnt_eat_mutex);
-        ft_wait(arg->time_to_eat);
-        pthread_mutex_unlock(&(arg->fork[philo->fork_right]));
-    }
-    pthread_mutex_unlock(&(arg->fork[philo->fork_left]));
+	pthread_mutex_lock(&(arg->fork[(philo->fork_left)]));
+	display(arg, "has taken a fork", philo->id);
+	if (arg->num_philo > 1)
+	{
+		pthread_mutex_lock(&(arg->fork[philo->fork_right]));
+		display(arg, "has taken a fork", philo->id);
+		display(arg, "is eating", philo->id);
+		pthread_mutex_lock(&philo->last_meal_mutex);
+		philo->last_meal = get_time();
+		pthread_mutex_unlock(&philo->last_meal_mutex);
+		pthread_mutex_lock(&philo->cnt_eat_mutex);
+		philo->cnt_eat += 1;
+		pthread_mutex_unlock(&philo->cnt_eat_mutex);
+		ft_wait(arg->time_to_eat);
+		pthread_mutex_unlock(&(arg->fork[philo->fork_right]));
+	}
+	pthread_mutex_unlock(&(arg->fork[philo->fork_left]));
 }
 
-void    philo_sleep(t_argset *arg, t_philo *philo)
+void	philo_sleep(t_argset *arg, t_philo *philo)
 {
-    display(arg, "is sleeping", philo->id);
-    ft_wait(arg->time_to_sleap);
+	display(arg, "is sleeping", philo->id);
+	ft_wait(arg->time_to_sleap);
 }
