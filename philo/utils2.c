6aa1b03e2f7a7b1eba0dc2960877b002fbdd0eb3
GANZIII <108858693+GANZIII@users.noreply.github.com>
2023-01-05 17:31:03 +0900

    final

--- philo/utils2.c
+++ philo/utils2.c
@@ -1,62 +1,54 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utils2.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jijoo <jijoo@student.42seoul.kr>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/30 22:39:01 by jijoo             #+#    #+#             */
-/*   Updated: 2023/01/04 17:51:36 by jijoo            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "./include/philosopher.h"
 
-void    display(t_argset *arg, char *str, int id)
+void	display(t_argset *arg, char *str, int id)
 {
-    long int    now;
+	long int	now;
 
-    now = get_time();
-    pthread_mutex_lock(&(arg->display_mutex));
-    if (!check_update(arg, CHECK))
-        printf("%ld %d %s\n", now - arg->initial_time, id + 1, str);
-    pthread_mutex_unlock(&(arg->display_mutex));
+	now = get_time();
+	pthread_mutex_lock(&(arg->display_mutex));
+	if (!check_update(arg, CHECK))
+		printf("%ld %d %s\n", now - arg->initial_time, id + 1, str);
+	if (!ft_strncmp(str, "is died", 7))
+	{
+		check_update(arg, UPDATE);
+	}
+	pthread_mutex_unlock(&(arg->display_mutex));
 }
 
-void    ft_wait(long int ms)
+void	ft_wait(long int ms)
 {
-    long int    start;
+	long int	start;
 
-    start = get_time();
-    while ((get_time() - start) < ms)
-        usleep(ms / 10);
+	start = get_time();
+	while ((get_time() - start) < ms)
+		usleep(ms / 10);
 }
 
-void    dobby_fork(t_argset *arg, int idx)
+void	dobby_fork(t_argset *arg, int idx)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < idx)
-    {
-        pthread_mutex_unlock(&(arg->fork[i]));
-        pthread_mutex_destroy(&(arg->fork[i]));
-        i++;
-    }
-    free(arg->fork);
+	i = 0;
+	while (i < idx)
+	{
+		pthread_mutex_unlock(&(arg->fork[i]));
+		pthread_mutex_destroy(&(arg->fork[i]));
+		i++;
+	}
+	free(arg->fork);
 }
 
-void    dobby_philo(t_philo *philo, int idx)
+void	dobby_philo(t_philo *philo, int idx)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < idx)
-    {
-        pthread_mutex_destroy(&philo->last_meal_mutex);
-        pthread_mutex_destroy(&philo->cnt_eat_mutex);
-        pthread_join(philo[i].thread, 0);
-        i++;
-    }
-    free(philo);
+	i = 0;
+	while (i < idx)
+	{
+		pthread_join(philo[i].thread, 0);
+		pthread_mutex_destroy(&philo->last_meal_mutex);
+		pthread_mutex_destroy(&philo->cnt_eat_mutex);
+		i++;
+	}
+	free(philo);
 }
